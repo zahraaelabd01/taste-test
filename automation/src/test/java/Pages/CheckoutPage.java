84de98a73531c5503434ed6a5e7b9c755eca0ce3
Sarah <sarabassem98@gmail.com>
2026-07-06 02:00:19 +0300

    verify empty cart checkout prevention

--- automation/src/test/java/Pages/CheckoutPage.java
+++ automation/src/test/java/Pages/CheckoutPage.java
@@ -11,44 +11,13 @@ public class CheckoutPage {
         this.driver = driver;
     }
 
-    By checkoutBtn = By.xpath("//a[contains(text(),'Checkout')]");
-    By firstName = By.id("input-payment-firstname");
-    By lastName = By.id("input-payment-lastname");
-    By address = By.id("input-payment-address-1");
-    By city = By.id("input-payment-city");
-    By postCode = By.id("input-payment-postcode");
-    By confirmOrder = By.id("button-confirm");
-    By successMessage = By.cssSelector(".alert-success");
+    By checkoutButton = By.linkText("Checkout");
+
+    public void openWebsite() {
+        driver.get("https://ecommerce-playground.lambdatest.io/");
+    }
 
     public void clickCheckout() {
-        driver.findElement(checkoutBtn).click();
-    }
-
-    public void enterFirstName(String name) {
-        driver.findElement(firstName).sendKeys(name);
-    }
-
-    public void enterLastName(String name) {
-        driver.findElement(lastName).sendKeys(name);
-    }
-
-    public void enterAddress(String add) {
-        driver.findElement(address).sendKeys(add);
-    }
-
-    public void enterCity(String cityName) {
-        driver.findElement(city).sendKeys(cityName);
-    }
-
-    public void enterPostCode(String code) {
-        driver.findElement(postCode).sendKeys(code);
-    }
-
-    public void confirmOrder() {
-        driver.findElement(confirmOrder).click();
-    }
-
-    public String getSuccessMessage() {
-        return driver.findElement(successMessage).getText();
+        driver.findElement(checkoutButton).click();
     }
 }
--- automation/src/test/java/tests/CheckoutTests.java
+++ automation/src/test/java/tests/CheckoutTests.java
@@ -3,35 +3,35 @@ package tests;
 import BaseTest.BaseTest;
 import Pages.CartPage;
 import Pages.CheckoutPage;
-
+import Pages.HomePage;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class CheckoutTests extends BaseTest {
 
-    @BeforeMethod
-    public void openHome() {
+    private CheckoutPage checkoutPage;
 
+    @BeforeMethod
+    public void initializePages() {
+
+        homePage = new HomePage(driver);
         cartPage = new CartPage(driver);
         checkoutPage = new CheckoutPage(driver);
 
-        cartPage.openHome();
     }
 
+    // TC: Verify user cannot proceed to checkout with empty cart
     @Test
-    public void EmptyCartTest() throws InterruptedException {
+    public void verifyUserCannotCheckoutWithEmptyCart() {
 
         cartPage.openDirectly();
 
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
         Assert.assertTrue(cartPage.isEmptyCartMessageDisplayed());
 
+        Assert.assertEquals(
+                cartPage.getPageHeadingText(),
+                "Shopping Cart"
+        );
     }
-
 }
