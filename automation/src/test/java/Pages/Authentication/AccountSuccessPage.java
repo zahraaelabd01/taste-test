0a3f2e58106437984a30a689f70844b27e0af745
Habeeba <habeebasameh1112@gmail.com>
2026-07-06 01:41:12 +0300

    Register page + valid register test is done

---
+++ automation/src/test/java/Pages/Authentication/AccountSuccessPage.java
@@ -0,0 +1,29 @@
+package Pages.Authentication;
+
+import Pages.MyAccount.MyAccountPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AccountSuccessPage {
+    WebDriver driver;
+    By SuccessMsg = By.cssSelector(".page-title.my-3");
+    String ExpSuccessMsg="Your Account Has Been Created!";
+    By continueBtn = By.cssSelector(".buttons.mb-4");
+
+    public AccountSuccessPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getSuccessMsg() {
+        return driver.findElement(SuccessMsg).getText();
+    }
+
+    public MyAccountPage ClickContinue() {
+        driver.findElement(continueBtn).click();
+        return new MyAccountPage(driver);
+    }
+
+    public String getExpSuccessMsg() {
+        return ExpSuccessMsg;
+    }
+}
