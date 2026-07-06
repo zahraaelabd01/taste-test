2342ae3295884742c85d5c44a3136f4c9c8c48c9
Habeeba <habeebasameh1112@gmail.com>
2026-07-06 17:38:34 +0300

    (InvalidDataType + DuplicateEmail) register page test

--- automation/src/test/java/tests/AuthTest/RegisterTest.java
+++ automation/src/test/java/tests/AuthTest/RegisterTest.java
@@ -24,7 +24,7 @@ public class RegisterTest extends BaseTest.BaseTest {
         registerPage.clickPrivacyPolicy();
         AccountSuccessPage successPage=registerPage.clickContinue();
         Assert.assertEquals(successPage.getSuccessMsg(),successPage.getExpSuccessMsg());
-        System.out.println("valid Register test pass");
+        System.out.println("---valid Register test pass---");
         System.out.println("************************");
     }
     @Test(description = "TC_AUTH_005")
@@ -46,9 +46,50 @@ public class RegisterTest extends BaseTest.BaseTest {
         Assert.assertEquals(registerPage.getActualPassErrorMsg(),
                 registerPage.getExpectedPassErrorMsg(),
                 "Password error message did not match!");
-        System.out.println("Empty fields test pass ");
+        System.out.println("---Empty fields Error Messages test pass--- ");
     }
+    @Test(description = "TC_AUTH_009")
+    public void PasswordMismatchTest() {
+        registerPage.setFirstName("Test");
+        registerPage.setLastName("one");
+        registerPage.setEmail("test999@email.com");
+        registerPage.setTelephone("13802603245");
+        registerPage.setPassword("12345");
+        registerPage.setConfirmPassword("DifferentPass");
+        registerPage.clickPrivacyPolicy();
+        registerPage.clickContinue();
+        Assert.assertEquals(registerPage.getActualConfirmPassErrorMsg(),
+                registerPage.getExpectedConfirmPassErrorMsg());
+        System.out.println("---Password mismatch test pass--- ");
+        System.out.println("************************");
 
-
-
+    }
+    @Test(description = "TC_REGISTER_006")
+    public void DuplicateEmailTest(){
+        registerPage.setFirstName("Test");
+        registerPage.setLastName("One");
+        registerPage.setEmail("vorow6112118@parsitv.com");
+        registerPage.setTelephone("13802603245");
+        registerPage.setPassword("1234");
+        registerPage.setConfirmPassword("1234");
+        registerPage.clickPrivacyPolicy();
+        registerPage.clickContinue();
+        Assert.assertEquals(registerPage.getRegisteredEmailErrorMsg(),registerPage.getExpRegisteredEmailErrorMsg());
+        System.out.println("---Duplicate Email test pass---");
+        System.out.println("************************");
+    }
+    @Test (enabled = false, description = "TC_AUTH_003 - Bug:System accepts invalid data in registration fields")
+    public void InvalidDataTypeTest() {
+        registerPage.setFirstName("1234+4");
+        registerPage.setLastName("h---abc");
+        registerPage.setEmail("test9999@email.com");
+        registerPage.setTelephone("abcdefghij");
+        registerPage.setPassword("12345");
+        registerPage.setConfirmPassword("12345");
+        registerPage.clickPrivacyPolicy();
+        registerPage.clickContinue();
+        Assert.assertEquals(registerPage.getActualFNameErrorMsg(),registerPage.getExpectedFNameErrorMsg());
+        Assert.assertEquals(registerPage.getActualLNameErrorMsg(),registerPage.getExpectedLNameErrorMsg());
+        Assert.assertEquals(registerPage.getActualTelephoneErrorMsg(),registerPage.getExpectedTelephoneErrorMsg());
+    }
 }
